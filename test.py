1ff59c45dd94179f0f293ca006a46e795c3e8b09
sharon12312 <sharon12312mor@gmail.com>
2018-01-14 22:16:23 +0200

    changing nueral network

--- test.py
+++ test.py
@@ -1,20 +1,9 @@
-from keras.applications.inception_resnet_v2 import preprocess_input
-from keras.applications.inception_resnet_v2 import InceptionResNetV2
-from skimage.transform import resize
-from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose
 from keras.models import model_from_json
-from keras.layers import Activation, Dense, Dropout, Flatten
-from keras.layers.normalization import BatchNormalization
-from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from skimage.color import rgb2lab, lab2rgb, rgb2gray, xyz2lab
 from skimage.io import imsave
 import numpy as np
-import os
-from PIL import Image
 from skimage.color import rgb2lab, lab2rgb, rgb2gray, gray2rgb
-import random
-import tensorflow as tf
+from PIL import Image, ImageFilter
 
 # Load json and create model
 json_file = open('model.json', 'r')
@@ -22,32 +11,40 @@ loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 
-# load weights into new model
+# Load weights into new model
 loaded_model.load_weights("./Models/color_tensorflow_end.h5")
 print("Loaded model from disk..")
 
 loaded_model.compile(optimizer='adam', loss='mse')
 
-# image = img_to_array(load_img('./Test/image.jpg'))
-# image = Image.open('./Test/image.jpg')
-# image = image.resize((96,96))
-
-image = img_to_array(load_img('./Images/image_3.jpg'))
+# Load Train32
+image = img_to_array(load_img('./Images/image_00004.jpg'))
 image = np.array(image, dtype=float)
-
 X = rgb2lab(1.0/255*image)[:,:,0]
-Y = rgb2lab(1.0/255*image)[:,:,1:]
-Y /= 128
 X = X.reshape(1, 32, 32, 1)
-Y = Y.reshape(1, 32, 32, 2)
 
-print(loaded_model.evaluate(X, Y, batch_size=1))
+# Open image with Image object for resize later
+img = Image.open('./Images/image_00004.jpg')
+
+# Predict
 output = loaded_model.predict(X)
 output *= 128
 
+# Resize black and white photo to 96X96X1
+new_img = img.resize((96, 96), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+new_img = np.array(new_img, dtype=float)
+X_96 = rgb2lab(1.0/255*new_img)[:,:,0]
+X_96= X_96.reshape(1, 96, 96, 1)
+
 # Output colorizations
-cur = np.zeros((32, 32, 3))
-cur[:,:,0] = X[0][:,:,0]
+cur = np.zeros((96, 96 ,3))
+cur[:,:,0] = X_96[0][:,:,0]
 cur[:,:,1:] = output[0]
-imsave("./Results/img_result.png", lab2rgb(cur))
-# imsave("./result/img_gray_version.png", rgb2gray(lab2rgb(cur)))
+
+# Prepare photo to evaluate
+Y = rgb2lab(1.0/255*lab2rgb(cur))[:,:,1:]
+Y /= 128
+Y = Y.reshape(1, 96, 96, 2)
+
+print(loaded_model.evaluate(X, Y, batch_size=1))
+imsave("./Results/img_result8.jpg", lab2rgb(cur))
--- train.py
+++ train.py
@@ -1,47 +1,31 @@
-from keras.applications.inception_resnet_v2 import preprocess_input
-from keras.applications.inception_resnet_v2 import InceptionResNetV2
-from skimage.transform import resize
-from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose
-from keras.models import model_from_json
-from keras.layers import Activation, Dense, Dropout, Flatten
-from keras.layers.normalization import BatchNormalization
+from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose,UpSampling3D,Conv3D
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from skimage.color import rgb2lab, lab2rgb, rgb2gray, xyz2lab
-from skimage.io import imsave
 import numpy as np
 import os
-from PIL import Image
 from skimage.color import rgb2lab, lab2rgb, rgb2gray, gray2rgb
-import random
-import tensorflow as tf
+
+# Parameters of Model
+Batch_size = 20
+Epochs = 50
 
 # Get images
 X = []
 Y = []
-for filename in os.listdir('./Train/'):
-    inp = img_to_array(load_img('./Train/'+filename))
+for filename in os.listdir('./Train32/'):
+    inp = img_to_array(load_img('./Train32/'+filename))
     inp = np.array(inp, dtype=float)
     inp = rgb2lab(1.0/255*inp)[:,:,0]
     inp = inp.reshape(1,32,32,1)
     X.append(inp)
-    out = img_to_array(load_img('./Train/'+filename))
+    out = img_to_array(load_img('./Train96/'+filename))
     out = np.array(out, dtype=float)
     out = rgb2lab(1.0 / 255 * out)[:, :, 1:]
     out /= 128
-    out = out.reshape(1,32,32,2)
+    out = out.reshape(1,96,96,2)
     Y.append(out)
 X = np.array(X, dtype=float)
 Y = np.array(Y, dtype=float)
-
-# image = img_to_array(load_img('./Train/11746276_de3dec8201.jpg'))
-# image = np.array(image, dtype=float)
-
-# X = rgb2lab(1.0/255*image)[:,:,0]
-# Y = rgb2lab(1.0/255*image)[:,:,1:]
-# Y /= 128
-# X = X.reshape(1, 32, 32, 1)
-# Y = Y.reshape(1, 32, 32, 2)
 
 # Building the neural network
 model = Sequential()
@@ -53,19 +37,21 @@ model.add(Conv2D(16, (3, 3), activation='relu', padding='same', strides=2))
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same', strides=2))
 model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
 model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((3, 3)))
 model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
 
 # Finish model
 model.compile(optimizer='adam', loss='mse')
+print(model.output)
 
-# Train Model
-def generator(features, labels, batch_size):
+# Train96 Model
+def generator(features,labels, batch_size):
  batch_features = np.zeros((batch_size, 32, 32, 1))
- batch_labels = np.zeros((batch_size, 32, 32, 2))
+ batch_labels = np.zeros((batch_size, 96, 96, 2))
  while True:
    for i in range(batch_size):
      index= i
@@ -73,7 +59,8 @@ def generator(features, labels, batch_size):
      batch_labels[i] = labels[index]
    yield batch_features, batch_labels
 
-model.fit_generator(generator(X, Y, 150), epochs=2000, steps_per_epoch=1, verbose=1)
+# Train Model - learning rate inside 'adam' optimizer
+model.fit_generator(generator(X ,Y,Batch_size), epochs=Epochs, steps_per_epoch=1, verbose=1)
 
 # Save model
 model_json = model.to_json()
@@ -83,38 +70,16 @@ model.save_weights("./Models/color_tensorflow_end.h5")
 
 print('Finished to train dataset.')
 
-# # Load json and create model
-# json_file = open('model.json', 'r')
-# loaded_model_json = json_file.read()
-# json_file.close()
-# loaded_model = model_from_json(loaded_model_json)
-#
-# # load weights into new model
-# loaded_model.load_weights("./Models/color_tensorflow_end.h5")
-# print("Loaded model from disk..")
-#
-# loaded_model.compile(optimizer='adam', loss='mse')
+# Print Params & write to Log file
+print('---------------------')
+print('Model - Parameters:')
+print('Batch Size: ', Batch_size)
+print('Number of epoches: ', Epochs)
+print('---------------------')
 
-# image = img_to_array(load_img('./Test/image.jpg'))
-# image = Image.open('./Test/image.jpg')
-# image = image.resize((96,96))
-
-# image = img_to_array(load_img('./Images/image_2.jpg'))
-# image = np.array(image, dtype=float)
-#
-# X = rgb2lab(1.0/255*image)[:,:,0]
-# Y = rgb2lab(1.0/255*image)[:,:,1:]
-# Y /= 128
-# X = X.reshape(1, 32, 32, 1)
-# Y = Y.reshape(1, 32, 32, 2)
-#
-# print(model.evaluate(X, Y, batch_size=1))
-# output = model.predict(X)
-# output *= 128
-
-# # Output colorizations
-# cur = np.zeros((32, 32, 3))
-# cur[:,:,0] = X[0][:,:,0]
-# cur[:,:,1:] = output[0]
-# imsave("./Results/img_result.png", lab2rgb(cur))
-# imsave("./result/img_gray_version.png", rgb2gray(lab2rgb(cur)))
+# Write parameters to a local file
+with open('./Logs/params.txt', 'a') as the_file:
+    the_file.write('Train:'  + '\n')
+    the_file.write('Batch Size: ' +  str(batch_size) + '\n')
+    the_file.write('Number of epoches: ' + str(Epochs) + '\n')
+    the_file.write('----------------\n')
